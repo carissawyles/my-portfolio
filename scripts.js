69491c90ed7848222b7e84f37eecbde189045b8a
Carissa Wyles <cwyles725@gmail.com>
2024-08-15 14:15:54 -0700

    updated form error handling

--- scripts.js
+++ scripts.js
@@ -5,20 +5,23 @@ function checkdata(){
       //Create references to the input elements we wish to validate
       var name = document.getElementById("name");
       var email_address = document.getElementById("email");
+	  document.getElementById("nameError").style.display = 'none';
+	  document.getElementById("emailError").style.display = 'none';
       
       //Check if username field is empty
       if(name.value == ""){
-            alert("Please enter your name");
+            document.getElementById("nameError").style.display = 'block';
             name.focus();
             return false;
       }
      //Check if email field is empty
       if(email_address.value == ""){
-            alert("Please enter your email");
+            document.getElementById("emailError").style.display = 'block';
             email_address.focus();
             return false;
       }
      //If all is well show pop-up message and return true.
+	 
 	  showPopup(true);
       return true;
 }
